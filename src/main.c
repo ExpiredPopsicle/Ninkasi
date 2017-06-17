57f2cc8645dfb985aee9bb43644f34c292dacde2
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-06-16 19:26:12 -0700

    asdf

--- src/main.c
+++ src/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
+typedef int int32_t;
+
+enum LispValueType
+{
+    LISP_INT,
+    LISP_FLOAT,
+    LISP_CONS
+};
+
+struct LispValue
+{
+    enum LispValueType type;
+
+    union
+    {
+        int32_t intData;
+        float floatData;
+
+        struct
+        {
+            struct LispValue *car;
+            struct LispValue *cdr;
+        };
+    };
+};
+
 int main(int argc, char *argv[])
 {
+
+
+
     return 0;
 }
 
