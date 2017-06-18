a28810e97ab04810988de87a52a342dae72157d9
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-06-18 01:14:23 -0700

    Started tokenizing work.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,107 @@
 
 // ----------------------------------------------------------------------
 
+enum TokenType
+{
+    TOKENTYPE_INTEGER,
+    TOKENTYPE_FLOAT,
+    TOKENTYPE_PLUS,
+    TOKENTYPE_INCREMENT,
+    TOKENTYPE_PAREN_OPEN,
+    TOKENTYPE_PAREN_CLOSE
+};
+
+struct Token
+{
+    enum TokenType type;
+    char *string;
+};
+
+bool isWhitespace(char c)
+{
+    if(c == ' ' || c == '\n' || c == '\t' || c == '\r') {
+        return true;
+    }
+    return false;
+}
+
+bool isNumber(char c)
+{
+    return (c >= '0' && c <= '9');
+}
+
+void addToken(enum TokenType type, const char *str)
+{
+    // TODO
+    printf("Token (%d): %s\n", type, str);
+}
+
+void tokenize(const char *str)
+{
+    uint32_t len = strlen(str);
+    uint32_t i = 0;
+
+    while(i < len) {
+
+        // Skip whitespace.
+        while(i < len && isWhitespace(str[i])) {
+            i++;
+        }
+
+        if(str[i] == '(') {
+
+            addToken(TOKENTYPE_PAREN_OPEN, "(");
+
+        } else if(str[i] == ')') {
+
+            addToken(TOKENTYPE_PAREN_CLOSE, ")");
+
+        } else if(str[i] == '+') {
+
+            // Check for "++".
+            if(str[i+1] == '+') {
+                addToken(TOKENTYPE_INCREMENT, "++");
+                i++;
+            } else {
+                addToken(TOKENTYPE_PLUS, "+");
+            }
+
+        } else if(isNumber(str[i])) {
+
+            // Scan until we find the end of the number.
+            uint32_t numberStart = i;
+            bool isFloat = false;
+            char tmp[256];
+
+            while(i < len &&
+                (isNumber(str[i]) || str[i] == '.') &&
+                (i - numberStart) < sizeof(tmp) - 1)
+            {
+                tmp[i - numberStart] = str[i];
+                if(str[i] == '.') {
+                    isFloat = true;
+                }
+                i++;
+            }
+
+            // Add null terminator.
+            tmp[i - numberStart] = 0;
+
+            // Back up one character, so we don't eat the first
+            // character of whatever comes next.
+            i--;
+
+            addToken(isFloat ? TOKENTYPE_FLOAT : TOKENTYPE_INTEGER, tmp);
+        }
+
+        i++;
+    }
+}
+
+
+
+
+
 int main(int argc, char *argv[])
 {
     struct VMStack stack;
@@ -29,6 +130,13 @@ int main(int argc, char *argv[])
     vmstack_dump(&stack);
 
     vmstack_destroy(&stack);
+
+
+
+
+
+    printf("Tokenize test...\n");
+    tokenize("(123 + 456) * 789++");
 
 
     return 0;
