43ef0ee51e497a57b7d8d5b6da30fb48103f2154
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-06-18 15:34:55 -0700

    Shifting and reducing seems to work.

--- src/main.c
+++ src/main.c
@@ -244,13 +244,17 @@ void dbgPop(void)
 // TODO: Add to error state instead of printing.
 #define EXPECT_AND_SKIP(x)                                      \
     do {                                                        \
-        if(!currentToken || currentToken->type != x) {          \
-            dbgWriteLine("Error: Expected token type %d", x); \
+        if(!(*currentToken) || (*currentToken)->type != x) {    \
+            dbgWriteLine("Error: Expected token type %d", x);   \
             return NULL;                                        \
         }                                                       \
-        currentToken = currentToken->next;                      \
+        (*currentToken) = (*currentToken)->next;                \
     } while(0)
 
+#define NEXT_TOKEN()                                \
+    do {                                            \
+        (*currentToken) = (*currentToken)->next;    \
+    } while(0)
 
 bool getPrecedence(enum TokenType t)
 {
@@ -274,45 +278,167 @@ bool getPrecedence(enum TokenType t)
 }
 
 
-
-
-struct Token *parseExpression(struct Token *currentToken)
+struct ExpressionAstNode
 {
-    while(!isExpressionEndingToken(currentToken)) {
+    struct Token *opOrValue;
+    struct ExpressionAstNode *children[2];
+    struct ExpressionAstNode *stackNext;
+};
 
-        // Deal with prefix operators.
-        while(isPrefixOperator(currentToken)) {
-            dbgWriteLine("Parse prefix operator: %s", currentToken->str);
-            currentToken = currentToken->next;
+#define MAKE_OP(x)                                                      \
+    struct ExpressionAstNode *astNode = malloc(sizeof(struct ExpressionAstNode)); \
+    astNode->stackNext = opStack;                                       \
+    astNode->children[0] = NULL;                                        \
+    astNode->children[1] = NULL;                                        \
+    astNode->opOrValue = x;
+
+#define PUSH_OP(x)                              \
+    do {                                        \
+        MAKE_OP(x);                             \
+        opStack = astNode;                      \
+    } while(0)
+
+#define PUSH_VAL(x)                             \
+    do {                                        \
+        MAKE_OP(x);                             \
+        valueStack = astNode;                   \
+    } while(0)
+
+
+void dumpExpressionAstNode(struct ExpressionAstNode *node)
+{
+    if(!node) {
+        printf("<NULL>");
+        return;
+    }
+
+    if(isPrefixOperator(node->opOrValue) && !node->children[1]) {
+
+        printf(node->opOrValue->str);
+        printf("(");
+        dumpExpressionAstNode(node->children[0]);
+        printf(")");
+
+    } else {
+
+        if(node->children[0]) {
+            dumpExpressionAstNode(node->children[0]);
+        } else {
+            // printf(" NULL1");
+        }
+
+        printf("%s", node->opOrValue->str);
+
+        if(node->children[1]) {
+            dumpExpressionAstNode(node->children[1]);
+        } else {
+            // printf(" NULL2");
+        }
+
+    }
+}
+
+void reduce(
+    struct ExpressionAstNode **opStack,
+    struct ExpressionAstNode **valueStack)
+{
+    struct ExpressionAstNode *valNode1 = (*valueStack)->stackNext;
+    struct ExpressionAstNode *valNode2 = (*valueStack);
+    struct ExpressionAstNode *opNode   = (*opStack);
+
+    assert(valNode1 && valNode2 && opNode);
+
+    // Pop off the values.
+    (*valueStack) = (*valueStack)->stackNext->stackNext;
+    valNode1->stackNext = NULL;
+    valNode2->stackNext = NULL;
+    (*opStack) = opNode->stackNext;
+    opNode->stackNext = NULL;
+
+    // Assign children and push the operation onto the
+    // value stack.
+    opNode->children[0] = valNode1;
+    opNode->children[1] = valNode2;
+    opNode->stackNext = (*valueStack);
+    (*valueStack) = opNode;
+}
+
+struct ExpressionAstNode *parseExpression(struct Token **currentToken)
+{
+    struct ExpressionAstNode *opStack = NULL;
+    struct ExpressionAstNode *valueStack = NULL;
+
+    while(!isExpressionEndingToken(*currentToken)) {
+
+        struct ExpressionAstNode *lastPrefixOp   = NULL;
+        struct ExpressionAstNode *firstPrefixOp  = NULL;
+        struct ExpressionAstNode *lastPostfixOp  = NULL;
+        struct ExpressionAstNode *firstPostfixOp = NULL;
+        struct ExpressionAstNode *valueNode      = NULL;
+
+        // Deal with prefix operators. Build up a list of them.
+        while(isPrefixOperator(*currentToken)) {
+
+            struct ExpressionAstNode *prefixNode = malloc(sizeof(struct ExpressionAstNode));
+            memset(prefixNode, 0, sizeof(*prefixNode));
+
+            // Add it to the end of our list of prefix operations.
+            prefixNode->opOrValue = *currentToken;
+            if(lastPrefixOp) {
+                lastPrefixOp->children[0] = prefixNode;
+            }
+            lastPrefixOp = prefixNode;
+
+            // Set it as the first if there's nothing else there.
+            if(!firstPrefixOp) {
+                firstPrefixOp = prefixNode;
+            }
+
+            dbgWriteLine("Parse prefix operator: %s", (*currentToken)->str);
+
+            NEXT_TOKEN();
         }
 
         // Parse a value or sub-expression.
-        if(currentToken->type == TOKENTYPE_PAREN_OPEN) {
+        if((*currentToken)->type == TOKENTYPE_PAREN_OPEN) {
 
             // Parse sub-expression.
             dbgWriteLine("Parse sub-expression.");
             dbgPush();
-            currentToken = parseExpression(currentToken->next);
+            NEXT_TOKEN();
+            valueNode = parseExpression(currentToken);
             dbgPop();
 
+            // Error check.
+            if(!valueNode) {
+                dbgWriteLine("Error: Subexpression parse failure.");
+                return NULL;
+            }
+
             // Make sure we ended on a closing parenthesis.
-            if(!currentToken || !isSubexpressionEndingToken(currentToken)) {
+            if(!(*currentToken) || !isSubexpressionEndingToken(*currentToken)) {
                 // TODO: Raise error flag.
                 dbgWriteLine("Error: Bad expression end.");
                 return NULL;
             }
 
-            dbgWriteLine("Back from sub-expression. Current token: %s", currentToken->str);
+            dbgWriteLine("Back from sub-expression. Current token: %s", (*currentToken)->str);
 
             // Skip expression-ending token.
-            currentToken = currentToken->next;
+            NEXT_TOKEN();
 
         } else {
 
             // Parse the actual value.
-            if(!isExpressionEndingToken(currentToken)) {
-                dbgWriteLine("Parse value: %s", currentToken->str);
-                currentToken = currentToken->next;
+            if(!isExpressionEndingToken(*currentToken)) {
+
+                valueNode = malloc(sizeof(struct ExpressionAstNode));
+                memset(valueNode, 0, sizeof(*valueNode));
+                valueNode->opOrValue = *currentToken;
+
+                dbgWriteLine("Parse value: %s", (*currentToken)->str);
+                NEXT_TOKEN();
+
             } else {
                 // Prefix operator with no value. Why?
                 // TODO: Raise error flag.
@@ -322,16 +448,39 @@ struct Token *parseExpression(struct Token *currentToken)
         }
 
         // Deal with postfix operators.
-        while(isPostfixOperator(currentToken)) {
-            dbgWriteLine("Parse postfix operator: %s", currentToken->str);
+        while(isPostfixOperator(*currentToken)) {
+
+            struct ExpressionAstNode *postfixNode = malloc(sizeof(struct ExpressionAstNode));
+            memset(postfixNode, 0, sizeof(*postfixNode));
+
+            // Add it to the end of our list of postfix operations.
+            postfixNode->opOrValue = *currentToken;
+            if(lastPostfixOp) {
+                postfixNode->children[0] = lastPostfixOp;
+            }
+            lastPostfixOp = postfixNode;
+
+            // Set it as the first if there's nothing else there.
+            if(!firstPostfixOp) {
+                firstPostfixOp = postfixNode;
+            }
+
+            dbgWriteLine("Parse postfix operator: %s", (*currentToken)->str);
 
             // Handle index-into operator.
-            if(currentToken->type == TOKENTYPE_BRACKET_OPEN) {
+            if((*currentToken)->type == TOKENTYPE_BRACKET_OPEN) {
 
                 dbgWriteLine("Handling index-into operator.");
                 dbgPush();
-                currentToken = parseExpression(currentToken->next);
+                NEXT_TOKEN();
+                postfixNode->children[1] = parseExpression(currentToken);
                 dbgPop();
+
+                // Error-check.
+                if(!postfixNode->children[1]) {
+                    dbgWriteLine("Error: Subexpression parse failure.");
+                    return NULL;
+                }
 
                 EXPECT_AND_SKIP(TOKENTYPE_BRACKET_CLOSE);
 
@@ -342,34 +491,100 @@ struct Token *parseExpression(struct Token *currentToken)
                 // TODO: Raise error flag.
                 dbgWriteLine(
                     "Error: Unknown postfix operator (not implemented?): %s",
-                    currentToken->str);
+                    (*currentToken)->str);
                 return NULL;
 
             }
         }
 
+        // Attach prefix and postfix operations.
+        if(firstPostfixOp) {
+            assert(firstPostfixOp->children[0] == NULL);
+            firstPostfixOp->children[0] = valueNode;
+            valueNode = firstPostfixOp;
+        }
+        if(lastPrefixOp) {
+            assert(lastPrefixOp->children[0] == NULL);
+            lastPrefixOp->children[0] = valueNode;
+            valueNode = firstPrefixOp;
+        }
 
-        // TODO: Shift or reduce depending on last operator.
+        // Push this value.
+        valueNode->stackNext = valueStack;
+        valueStack = valueNode;
+
 
 
 
         // (Maybe) parse an operator.
 
         // Check to see if we're just done or not.
-        if(isExpressionEndingToken(currentToken)) {
-
+        if(isExpressionEndingToken(*currentToken)) {
             dbgWriteLine("Done parsing expression and maybe statement.");
             break;
 
         }
 
         // Not done yet. Parse the operator.
-        dbgWriteLine("Parse operator: %s", currentToken->str);
-        currentToken = currentToken->next;
+        dbgWriteLine("Parse operator: %s", (*currentToken)->str);
+
+        // Attempt to reduce.
+        if(opStack) {
+
+            if(getPrecedence((*currentToken)->type) > getPrecedence(opStack->opOrValue->type)) {
+
+                reduce(&opStack, &valueStack);
+                dbgWriteLine(
+                    "Reduced! %s > %s",
+                    (*currentToken)->str, opStack->opOrValue->str);
+
+            } else {
+                dbgWriteLine(
+                    "We should NOT reduce! %s <= %s",
+                    (*currentToken)->str, opStack->opOrValue->str);
+            }
+        }
+
+        PUSH_OP(*currentToken);
+
+        NEXT_TOKEN();
+
+        // TODO: Shift or reduce depending on last operator.
+    }
+
+
+    // TODO: Reduce all remaining operations?
+
+    // TODO: Check that stacks are empty.
+
+
+    while(opStack) {
+        reduce(&opStack, &valueStack);
+        dbgWriteLine("Reduced at end!");
+    }
+
+    printf("remaining values...\n");
+    {
+        struct ExpressionAstNode *n = valueStack;
+        while(n) {
+            dumpExpressionAstNode(n);
+            n = n->stackNext;
+            printf("\n");
+        }
+    }
+
+    printf("remaining ops...\n");
+    {
+        struct ExpressionAstNode *n = opStack;
+        while(n) {
+            dumpExpressionAstNode(n);
+            n = n->stackNext;
+            printf("\n");
+        }
     }
 
     dbgWriteLine("Expression parse success.");
-    return currentToken;
+    return NULL;
 }
 
 
@@ -415,10 +630,15 @@ int main(int argc, char *argv[])
     tokenList.first = NULL;
     tokenList.last = NULL;
     {
-        bool r = tokenize("(((123 + 456)[1 + 2][3])) * 789++ - -100 / ------300", &tokenList);
+        // bool r = tokenize("(((123 + 456)[1 + 2][3])) * 789++ - -100 / ------300", &tokenList);
+        bool r = tokenize("123 + 456 * 789 - -100 / ------300", &tokenList);
+        // bool r = tokenize("123 + 456", &tokenList);
         assert(r);
     }
-    parseExpression(tokenList.first);
+    {
+        struct Token *tokenPtr = tokenList.first;
+        parseExpression(&tokenPtr);
+    }
     destroyTokenList(&tokenList);
 
 
