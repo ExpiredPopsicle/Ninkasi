242ee793353e786701e914887143a7cf4b3e2ff0
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-08-13 20:10:02 -0700

    Added headers to everything, and a little bit of documentation.

--- src/nkmem.h
+++ src/nkmem.h
@@ -1,3 +1,57 @@
+// ----------------------------------------------------------------------
+//
+//        ▐ ▄ ▪   ▐ ▄ ▄ •▄  ▄▄▄· .▄▄ · ▪
+//       •█▌▐███ •█▌▐██▌▄▌▪▐█ ▀█ ▐█ ▀. ██
+//       ▐█▐▐▌▐█·▐█▐▐▌▐▀▀▄·▄█▀▀█ ▄▀▀▀█▄▐█·
+//       ██▐█▌▐█▌██▐█▌▐█.█▌▐█ ▪▐▌▐█▄▪▐█▐█▌
+//       ▀▀ █▪▀▀▀▀▀ █▪·▀  ▀ ▀  ▀  ▀▀▀▀ ▀▀▀
+//
+// ----------------------------------------------------------------------
+//
+//   Ninkasi 0.01
+//
+//   By Cliff "ExpiredPopsicle" Jolly
+//     https://expiredpopsicle.com
+//     https://intoxicoding.com
+//     expiredpopsicle@gmail.com
+//
+// ----------------------------------------------------------------------
+//
+//   Copyright (c) 2017 Clifford Jolly
+//
+//   Permission is hereby granted, free of charge, to any person
+//   obtaining a copy of this software and associated documentation files
+//   (the "Software"), to deal in the Software without restriction,
+//   including without limitation the rights to use, copy, modify, merge,
+//   publish, distribute, sublicense, and/or sell copies of the Software,
+//   and to permit persons to whom the Software is furnished to do so,
+//   subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be
+//   included in all copies or substantial portions of the Software.
+//
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//   SOFTWARE.
+//
+// -------------------------- END HEADER -------------------------------------
+
+// ----------------------------------------------------------------------
+// Memory (malloc/free) wrappers.
+// ----------------------------------------------------------------------
+//
+// This is the memory system that lets us track memory usage inside
+// the VM, and throw an error when the hosting-application-defined
+// memory limit OR system memory limit has been exhausted.
+//
+// Calls to user-defined malloc/free functions are handled inside this
+// system, too.
+
 #ifndef NINKASI_MEM_H
 #define NINKASI_MEM_H
 
