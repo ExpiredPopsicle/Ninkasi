2161929690a78f80adb5789d0958a0524385953c
ExpiredPopsicle <expiredpopsicle@gmail.com>
2019-09-10 22:58:09 -0700

    Base type parity work.

--- src/nktypes.h
+++ src/nktypes.h
@@ -44,62 +44,151 @@
 #ifndef NINKASI_BASETYPE_H
 #define NINKASI_BASETYPE_H
 
-// We should add more 16-bit and DOS configurations as we try out
-// different compilers.
+#include <limits.h>
 
-#ifdef __DOS__
-#   if defined(M_I86)
-        // 16-bit DOS under the Watcom compiler.
-#       define NK_16BIT
-#   elif defined(M_I386)
-        // 32-bit DOS.
-#       define NK_32BIT
-#   endif
+// ----------------------------------------------------------------------
+// Determine data model so we can figure out which type is really a
+// 32-bit int and 32-bit unsigned int.
+
+#if !defined(NKPP_32BIT) && !defined(NKPP_16BIT)
+
+#  if INT_MAX == 0x7fffffff && UINT_MAX == 0xffffffff
+
+     // ILP32 data model. Determined from INT_MAX and UINT_MAX
+#    define NKPP_32BIT
+
+#  elif defined(__ILP32__)
+
+     // ILP32 data model. The compiler actually tells us.
+#    define NKPP_32BIT
+
+#  elif defined(__LP32__)
+
+#    define NKPP_16BIT
+
+#  elif defined(__LP64__)
+
+     // LP64 data model. "int" and "unsigned int" are still 32-bit, and
+     // that's all we care about here.
+#    define NKPP_32BIT
+
+#  elif defined(__LLP64__)
+
+     // LLP64 data model. It's like LP64, but "long" is 32-bit and "long
+     // long" is 64-bit.
+#    define NKPP_32BIT
+
+#  elif defined(__ILP64__)
+
+     // We can figure this out later.
+#    error "Unsupported data model: ILP4"
+
+#  elif defined(__DOS__)
+
+     // We're not using a modern compiler or a modern OS here. Try to
+     // figure out the data model by some other means.
+
+#    if defined(M_I86)
+
+       // 16-bit DOS under the Watcom compiler.
+#      define NKPP_16BIT
+
+#    elif defined(M_I386)
+
+       // 32-bit DOS.
+#      define NKPP_32BIT
+
+#    endif
+
+#  else
+
+     // Our platorm detection has failed. Hopefully it's something
+     // where "int" and "unsigned int" are 32-bits.
+     //
+     // Add more cases here if we some day have more ways to
+     // differentiate.
+#    define NKPP_32BIT
+
+#  endif
+
+#endif // !defined(NKPP_32BIT) && !defined(NKPP_16BIT)
+
+// ----------------------------------------------------------------------
+// Actual type definitions
+
+// Ints.
+#ifdef NKPP_32BIT
+#  ifndef nkuint32_t
+#    define nkuint32_t unsigned int
+#  endif
+#  ifndef nkint32_t
+#    define nkint32_t signed int
+#  endif
+#  ifndef NK_PRINTF_INT32
+#    define NK_PRINTF_INT32 "%d"
+#  endif
+#  ifndef NK_PRINTF_UINT32
+#    define NK_PRINTF_UINT32 "%u"
+#  endif
+#elif defined(NKPP_16BIT)
+#  ifndef nkuint32_t
+#    define nkuint32_t unsigned long
+#  endif
+#  ifndef nkint32_t
+#    define nkint32_t signed long
+#  endif
+#  ifndef NK_PRINTF_INT32
+#    define NK_PRINTF_INT32 "%ld"
+#  endif
+#  ifndef NK_PRINTF_UINT32
+#    define NK_PRINTF_UINT32 "%lu"
+#  endif
 #else
-    // Other - assume 32-bit.
-#   define NK_32BIT
+#  error "No data model detected!"
 #endif
 
-#if defined(NK_32BIT)
-    typedef unsigned int nkuint32_t;
-    typedef int nkint32_t;
-#   define NK_PRINTF_INT32 "%d"
-#   define NK_PRINTF_UINT32 "%u"
-#else
-    typedef unsigned long int nkuint32_t;
-    typedef long int nkint32_t;
-#   define NK_PRINTF_INT32 "%ld"
-#   define NK_PRINTF_UINT32 "%lu"
-#endif
-#define NK_PRINTF_FLOATCHARSNEEDED 48
-#define NK_PRINTF_UINTCHARSNEED    20
-#define NK_PRINTF_INTCHARSNEED     21
-
-#ifndef nkuint8_t
-#define nkuint8_t unsigned char
+#ifndef NK_PRINTF_UINTCHARSNEED
+#  define NK_PRINTF_UINTCHARSNEED 11
 #endif
 
-#ifndef nkbool
-#define nkbool nkuint8_t
-#endif
-
-#ifndef nktrue
-#define nktrue ((nkbool)1)
-#endif
-
-#ifndef nkfalse
-#define nkfalse ((nkbool)0)
+#ifndef NK_PRINTF_INTCHARSNEED
+#  define NK_PRINTF_INTCHARSNEED 12
 #endif
 
 #ifndef NK_INVALID_VALUE
-#define NK_INVALID_VALUE (~(nkuint32_t)0)
+#  define NK_INVALID_VALUE (~(nkuint32_t)0)
 #endif
 
 #ifndef NK_UINT_MAX
-#define NK_UINT_MAX (~(nkuint32_t)0)
+#  define NK_UINT_MAX (~(nkuint32_t)0)
 #endif
 
-// TODO: FIXME: Replace manual overflow checks with these!
+// Bytes.
+#ifndef nkuint8_t
+#  define nkuint8_t unsigned char
+#endif
+
+// Booleans.
+#ifndef nkbool
+#  define nkbool nkuint8_t
+#endif
+
+#ifndef nktrue
+#  define nktrue ((nkbool)1)
+#endif
+
+#ifndef nkfalse
+#  define nkfalse ((nkbool)0)
+#endif
+
+// Floats.
+#ifndef NK_PRINTF_FLOATCHARSNEEDED
+#  define NK_PRINTF_FLOATCHARSNEEDED 48
+#endif
+
+// ----------------------------------------------------------------------
+// Overflow checks.
+
 #define NK_CHECK_OVERFLOW_UINT_ADD(a, b, result, overflow)  \
     do {                                                    \
         if((a) > NK_UINT_MAX - (b)) {                       \
@@ -115,6 +204,9 @@
         }                                                   \
         result = a * b;                                     \
     } while(0)
+
+// ----------------------------------------------------------------------
+// Ninkasi-specific forward defines.
 
 struct NKVM;
 struct NKValue;
